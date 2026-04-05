a3c9cca9055c2e2d2ac42bf19a57de1831c2bf64
Saba Sarfaraz <sarfarazsaba11@gmail.com>
2026-04-05 14:13:14 +0500

    Map function imp concept revission

---
+++ map.js
@@ -0,0 +1,52 @@
+// map function is applied on arra and it will return an array
+//map: array => array
+
+// value swill transorm in new modified values 
+
+let temps = [33,35, 32, 25, 24,26];
+
+// let modifyTemp = temps.map((temp, index ) =>{
+//     console.log(temp)
+//     return temp+10;
+// })
+
+// console.log(modifyTemp)
+
+//output 
+// modifyTemp[ 43, 45, 42, 35, 34, 36 ]
+
+// =======================================================
+
+//let modifyTemp = temps.map((temp, index) =>temp+10 );
+
+// if we need to return  only one line of code then we will omit { } and there is no need to use return keyword
+
+//console.log(modifyTemp)
+
+
+//================================================
+
+// sir example
+
+// let marks = [2,3,4,5,6];
+// let mappedArr = marks.map(x => {
+//     x = x*x;
+//     x+2;
+// })
+
+
+//return use nhi kia is lye undefine aye ga mappedArr mein
+
+
+//=================================================
+
+
+// let marks = [60, 30, 55, 40,90]
+// let marksArr = marks.map(x => x>5);
+// console.log(marksArr)
+
+
+//[ true, true, true, true, true ]
+
+//=================================================
+
