c2644615a3e59e666e12b690f18d93c1d6d9e116
Saba Sarfaraz <sarfarazsaba11@gmail.com>
2026-04-25 13:25:54 +0500

    Javascript practice : Topic objects from javascript info

---
+++ javascriptInfo/objects.js
@@ -0,0 +1,31 @@
+// Write the code, one line for each action:
+
+// Create an empty object user.
+const user = {} ;
+
+// Add the property name with the value John.
+
+user.name = "John" ;
+// Add the property surname with the value Smith.
+
+console.log(user)
+
+user.surname = "Smith";
+
+// Change the value of the name to Pete.
+
+console.log(user)
+
+user.name = "Pete" ;
+
+console.log(user)
+
+// Remove the property name from the object.
+
+delete user.name
+
+console.log(user)
+
+
+
+
